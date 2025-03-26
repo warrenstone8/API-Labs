7100b252da5ca7474b93b0665bf71ba0a09dfa0c
warren <20101336@mail.wit.ie>
2025-03-26 10:21:49 +0000

    added routing for posting a new task

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,12 +1,14 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import tasksRouter from './api/task';
+import tasksRouter from './api/tasks';
 
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
+
+app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
 
