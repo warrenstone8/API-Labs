391c307ac763cceb90409bcfd017b603253595e1
warren <20101336@mail.wit.ie>
2025-05-13 12:19:47 +0100

    Sign up Page

--- tasky-react/pages/signupPage.jsx
+++ tasky-react/pages/signupPage.jsx
@@ -1,17 +1,47 @@
-const SignUpPage = () => {
+import { useContext, useState } from "react";
+import { Navigate } from "react-router";
+import { AuthContext } from '../src/contexts/authContext';
 
-    return (
-      <>
-        <h2>SignUp page</h2>
-        <p>You must register a username and password to log in </p>
-        <input placeholder="user name" ></input><br />
-        <input type="password" placeholder="password" ></input><br />
-        <input type="password" placeholder="password again" ></input><br />
-        {/* Login web form  */}
-        <button>Register</button>
-      </>
-    );
-  };
+const SignUpPage = () => {
+  const context = useContext(AuthContext)
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordAgain, setPasswordAgain] = useState("");
+  const [registered, setRegistered] = useState(false);
   
-  export default SignUpPage;
+  const register = async () => {
+    let passwordRegEx = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+    const validPassword = passwordRegEx.test(password);
+
+    if (validPassword && password === passwordAgain) {
+      let result = await context.register(userName, password);
+      setRegistered(result);
+    }
+  }
+
+  if (registered === true) {
+    return <Navigate to="/login" />;
+  }
+
+  return (
+    <>
+      <h2>SignUp page</h2>
+      <p>You must register a username and password to log in. Usernames must be unique and passwords must contain a minimum of 8 characters (with at least one uppercase letter, one lowercase letter, and one symbol). </p>
+      <input value={userName} placeholder="user name" onChange={e => {
+        setUserName(e.target.value);
+      }}></input><br />
+      <input value={password} type="password" placeholder="password" onChange={e => {
+        setPassword(e.target.value);
+      }}></input><br />
+      <input value={passwordAgain} type="password" placeholder="password again" onChange={e => {
+        setPasswordAgain(e.target.value);
+      }}></input><br />
+      {/* Login web form  */}
+      <button onClick={register}>Register</button>
+    </>
+  );
+};
+
+export default SignUpPage;
+
   
