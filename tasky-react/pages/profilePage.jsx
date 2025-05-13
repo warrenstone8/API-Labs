f5144a48b14b85e77e8fb6540ffe42461de5bbd3
warren <20101336@mail.wit.ie>
2025-05-13 12:18:36 +0100

    Updated profile and start pages

--- tasky-react/pages/profilePage.jsx
+++ tasky-react/pages/profilePage.jsx
@@ -1,9 +1,16 @@
+import { useContext } from "react";
+import { AuthContext } from '../src/contexts/authContext';
 import { useNavigate } from "react-router";
 
 const ProfilePage = () => {
+    const context = useContext(AuthContext);
     const navigate = useNavigate();
   
-    return (
+    return context.isAuthenticated ? (
+        <p>
+            User profile: {context.userName}
+        </p>
+    ) : (
         <p>
             You must log in to see your profile! {" "}
             <button onClick={() => navigate('/login')}>Login</button>
--- tasky-react/pages/startPage.jsx
+++ tasky-react/pages/startPage.jsx
@@ -1,17 +1,20 @@
+import { useContext } from "react";
+import { AuthContext } from '../src/contexts/authContext';
 import { Link } from "react-router";
 
 const StartPage = () => {
+    const context = useContext(AuthContext);
   
-    return(
-        <>
-            <p>
-                Welcome to Tasky! View your <Link to="/tasks">Tasks</Link> or your <Link to="/profile">Profile</Link>.
-            </p>
-            <p>
-                <Link to="/login">Login</Link> or <Link to="/signup">Signup</Link> to create tasks!
-            </p>
-        </>
+    return context.isAuthenticated ? (
+        <p>
+            Welcome {context.userName}! View your <Link to="/tasks">Tasks</Link> or your <Link to="/profile">Profile</Link>.
+        </p>
+    ) : (
+        <p>
+            <Link to="/login">Login</Link> or <Link to="/signup">Signup</Link> to create tasks!
+        </p>
     );
   };
 
 export default StartPage;
+
